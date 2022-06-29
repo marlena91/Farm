82d473874eb2ec9b39a66c893dbe8a4ca546be9d
Marlena Żarczyńska <marlena.zarczynska@gmail.com>
2022-06-29 17:46:43 +0200

    Added animals manager

---
+++ src/animals/Sheep.java
@@ -0,0 +1,25 @@
+package animals;
+
+public class Sheep extends Animal implements Collectible, Feedable, Selable {
+
+    private String name;
+
+    public Sheep(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void collect() {
+        System.out.println("Golę owce");
+    }
+
+    @Override
+    public void feed() {
+        System.out.println("Karmię owce");
+    }
+
+    @Override
+    public void sell() {
+        System.out.println("Sprzedaje owce");
+    }
+}
